b15ed41858c4d9914c64cfe98498a95ffbb367b7
Moncef-Lak <76704813+Moncef-Lak@users.noreply.github.com>
2022-12-03 00:59:30 +0100

    add_new_img

--- src/pages/index/components/textSection.jsx
+++ src/pages/index/components/textSection.jsx
@@ -1,23 +1,30 @@
 import React, { useEffect, useRef } from "react";
-import { HiOutlineArrowNarrowRight } from 'react-icons/hi';
-import { CgScrollV } from 'react-icons/cg';
+import { HiOutlineArrowNarrowRight } from "react-icons/hi";
+import { CgScrollV } from "react-icons/cg";
 import { NavLink } from "react-router-dom";
-import img from '../../../assets/images/vignette.jpg';
+import img from "../../../assets/images/watch_background (2).webp";
 import video from "../../../assets/videos/video2.mp4";
 import { TweenMax, Power3 } from "gsap";
-import { wordsLetters, wordsLettersJsx } from "../../../functions/words_letters";
+import {
+  wordsLetters,
+  wordsLettersJsx,
+} from "../../../functions/words_letters";
 
 const TextSection = () => {
-
   let all_words = useRef(null);
   let button = useRef(null);
 
   // animation function
   useEffect(() => {
     all_words &&
-      TweenMax.staggerFrom(wordsLetters(all_words), .5, { x: 100, opacity: 0, ease: Power3.easeOut }, .1);
-    button?.classList.add('animation')
-  }, [])
+      TweenMax.staggerFrom(
+        wordsLetters(all_words),
+        0.5,
+        { x: 100, opacity: 0, ease: Power3.easeOut },
+        0.1
+      );
+    button?.classList.add("animation");
+  }, []);
 
   return (
     <section className="text-section">
@@ -26,21 +33,27 @@ const TextSection = () => {
           <source src={video} type="video/mp4" />
         </video>
       </div>
-      <div className="scroll" onClick={() => window.scrollTo(0, window.innerHeight - 50)}>
+      <div
+        className="scroll"
+        onClick={() => window.scrollTo(0, window.innerHeight - 50)}
+      >
         <CgScrollV />
       </div>
-      <div className="title" ref={e => all_words = e}>
-        {wordsLettersJsx('Pure, Automatic and very DZ watches')}
+      <div className="title" ref={(e) => (all_words = e)}>
+        {wordsLettersJsx("Pure, Automatic and very DZ watches")}
       </div>
-      <NavLink to='/store' ref={e => button = e} className="btn-prm index-btn">
+      <NavLink
+        to="/store"
+        ref={(e) => (button = e)}
+        className="btn-prm index-btn"
+      >
         Discover the watchs
         <div className="arrow-right">
           <HiOutlineArrowNarrowRight />
         </div>
       </NavLink>
     </section>
-
   );
-}
+};
 
 export default TextSection;
